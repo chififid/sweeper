172fa1354bdb72eb8bef5c7b9e54acb2783840dc
chififid <romanbdulev1@gmail.com>
2022-06-21 11:42:45 +0300

    Add some consts

---
+++ backend/constants/client.py
@@ -0,0 +1,8 @@
+CLIENT_BGS = [
+    "https://cdn.discordapp.com/attachments/777886925075382272/986279687707836456/unknown.png",
+    "https://cdn.discordapp.com/attachments/777886925075382272/986279558477119539/unknown.png",
+    "https://cdn.discordapp.com/attachments/777886925075382272/986279354914979890/unknown.png",
+    "https://cdn.discordapp.com/attachments/777886925075382272/986279172982866020/unknown.png",
+    "https://cdn.discordapp.com/attachments/777886925075382272/986279074676736060/unknown.png",
+    "https://cdn.discordapp.com/attachments/777886925075382272/986279042753892412/unknown.png",
+]
